0f054f999a619b6167ddd4fe753865fa89545321
Manisha Sahasrabudhe <manishas@jfrog.com>
2020-12-28 14:07:34 -0800

    committing python example

--- create-pypirc.sh
+++ create-pypirc.sh
@@ -6,7 +6,7 @@ index-servers = local
 [local]
 repository: https://smanisha.jfrog.io/artifactory/api/pypi/pipelines-pypi
 username: $res_demoArt_url
-password: $artifactory_password
+password: $int_demoArt_password_artifactory_password
 EOF
 
 echo "Created .pypirc file: Here it is: "
